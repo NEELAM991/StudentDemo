c6755cb2c4dbfbea0f6511524f7dd31cd84c37bc
Neelam <neelampatil991@gmail.com>
2017-01-09 15:54:23 +0530

    First Commit

---
+++ StudentDemo/app/src/main/java/com/example/neelam/studentdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.neelam.studentdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
